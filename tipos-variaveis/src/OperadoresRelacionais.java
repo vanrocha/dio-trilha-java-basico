b6d1f73834f39e628ffabe4d1648104f2e2729ed
vanrocha <rafaelrochajw@gmail.com>
2024-05-01 20:11:20 -0300

    Operadores Relacionais

---
+++ tipos-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,29 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        String nomeUm = "Rafael";
+        String nomeDois = "Rafael";
+
+        System.out.println(nomeUm == nomeDois);   
+        
+        
+        /*int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+            System.out.println("a nossa condiçao é verdadeira");
+        }
+
+        System.out.println("número UM é igual a número DOIS? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("número UM é diferente do número DOIS? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("número UM é maior do que o número DOIS? " + simNao);*/
+    }
+
+}
