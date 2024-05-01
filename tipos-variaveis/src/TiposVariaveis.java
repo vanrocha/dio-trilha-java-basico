ac0a291cef7ea98130b50bb29b9ca7c64ce4e1a0
vanrocha <rafaelrochajw@gmail.com>
2024-05-01 18:33:02 -0300

    Aula 4

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
     }
 }
