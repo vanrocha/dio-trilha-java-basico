411ec030e022ac37936273eb4189258c8decb13c
vanrocha <rafaelrochajw@gmail.com>
2024-05-01 19:52:55 -0300

    v1

--- tipos-variaveis/src/OperadorTernarios.java
+++ tipos-variaveis/src/OperadorTernarios.java
@@ -1,3 +1,19 @@
 public class OperadorTernarios {
+    public static void main(String[] args) {
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";    
+        
+        /*if (a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";*/    
+
+
+        System.out.println(resultado);    
+         
+    }
 
 }
