a140354463c856247efaa1b28d267e3f1e5dd2c3
vanrocha <rafaelrochajw@gmail.com>
2024-05-01 19:32:12 -0300

    boolean

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,21 @@
 public class Operadores {
     
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM "  +  " JAVA";
+        //int numero = 5;
+        //x repetição
+        //numero++;
+        //numero = numero +1;
 
-        System.err.println(nomeCompleto);
+        //System.out.println(numero -- );
+
+        //System.out.println(numero);
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        variavel = variavel;
+        System.out.println(variavel);
     }
 
 }
