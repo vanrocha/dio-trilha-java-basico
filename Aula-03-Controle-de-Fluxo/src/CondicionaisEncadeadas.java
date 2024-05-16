fd57b6c6a41cacaff6c610fdeeed4e1b817f6eeb
vanrocha <rafaelrochajw@gmail.com>
2024-05-16 11:10:56 -0300

    Condicionais Encadeadas

--- Aula-03-Controle-de-Fluxo/src/CondicionaisEncadeadas.java
+++ Aula-03-Controle-de-Fluxo/src/CondicionaisEncadeadas.java
@@ -1,15 +1,17 @@
 // ResultadoEscolar.java
 public class CondicionaisEncadeadas {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 10;
 
 	if (nota >= 7)
 		System.out.println("Aprovado");
-	else if (nota >= 5 && nota < 7)
+
+	else if (nota >= 5 && nota < 7)//Sempre tem que retornar true ou false
 		System.out.println("Recuperação");
+
 	else
 		System.out.println("Reprovado");
     }
 }
     
-}
+
