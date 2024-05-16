3ec1f92ae069ef6da5e720fa24f38e3822791eb6
vanrocha <rafaelrochajw@gmail.com>
2024-05-16 11:21:34 -0300

    Condição Ternária (Resultado Escolar)

--- Aula-03-Controle-de-Fluxo/src/CondicaoTernaria.java
+++ Aula-03-Controle-de-Fluxo/src/CondicaoTernaria.java
@@ -1,21 +1,23 @@
 
+/* 
 // Cenário 1 - Resultado Escolar
-public class CondiçãoTernaria {
+public class CondicaoTernaria {
     public static void main(String[] args) {
-		int nota = 7;
+		int nota = 5;
 		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
 		System.out.println(resultado);
 	}
     
 }
+*/
 
-/* 
+
+ 
 // Cenário 2 - Resultado Escolar
-public class CondiçãoTernaria {
+public class CondicaoTernaria {
 	public static void main(String[] args) {
 		int nota = 6;
 		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
 		System.out.println(resultado);
 	}
 }
-*/
