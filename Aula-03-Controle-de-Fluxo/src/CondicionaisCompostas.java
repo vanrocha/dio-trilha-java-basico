ef934ed5fb7d26f8d04ec3c4187e62b5078b1bd4
vanrocha <rafaelrochajw@gmail.com>
2024-05-16 09:53:52 -0300

    Condicionais Compostas

---
+++ Aula-03-Controle-de-Fluxo/src/CondicionaisCompostas.java
@@ -0,0 +1,13 @@
+// ResultadoEscolar.java
+public class CondicionaisCompostas {
+    public static void main(String[] args) {
+        int nota = 8;
+       
+        if(nota >= 7)
+         System.out.println("Aprovado");
+ 
+        else
+         System.out.println("Reprovado");
+    }
+    
+}
