5404cb110437909cae7dda640dd10c394a1b5548
vanrocha <rafaelrochajw@gmail.com>
2024-05-17 14:35:56 -0300

    For Each

---
+++ Estruturas-de-repeticao/src/ForEach.java
@@ -0,0 +1,15 @@
+// ExemploFor.java
+public class ForEach {
+    public static void main(String[] args) {
+
+        String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+        
+            //Forma abreviada
+        for(String aluno : alunos) {
+
+           System.out.println("Nome do aluno é: " + aluno);
+
+        }
+    
+    }
+    }
