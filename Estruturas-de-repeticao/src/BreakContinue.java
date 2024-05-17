be4d19794f7aeae941d3b96954781ceaab7e2585
vanrocha <rafaelrochajw@gmail.com>
2024-05-17 14:40:09 -0300

    Break e Continue

---
+++ Estruturas-de-repeticao/src/BreakContinue.java
@@ -0,0 +1,37 @@
+// Exemplo Break
+public class BreakContinue {
+	public static void main(String[] args) {
+	
+	for(int numero = 1; numero <=5; numero ++){
+
+		if(numero==3)
+
+			break;
+		
+		System.out.println(numero);
+		
+	}
+	//Qual a saída no console ?
+
+    }
+}
+
+/*
+ // Exemplo Continue
+public class ExemploBreakContinue {
+	public static void main(String[] args) {
+	
+	for(int numero = 1; numero <=5; numero ++){
+		if(numero==3)
+			continue;
+		
+		System.out.println(numero);
+		
+	}
+	//Qual a saída no console ?
+
+    }
+}
+
+
+ */
