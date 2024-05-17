219d442d1601d738841f3cb3b4e7705b9b688489
vanrocha <rafaelrochajw@gmail.com>
2024-05-17 14:21:08 -0300

    For

---
+++ Estruturas-de-repeticao/src/For.java
@@ -0,0 +1,10 @@
+// ExemploFor.java
+public class For {
+	public static void main(String[] args) {
+
+		for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+            
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		}
+	}
+}
