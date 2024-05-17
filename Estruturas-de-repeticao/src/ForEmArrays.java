01cfbb9f74f43f876d18f58ede958ca51ec5e6ca
vanrocha <rafaelrochajw@gmail.com>
2024-05-17 14:28:53 -0300

    For em Arrays

---
+++ Estruturas-de-repeticao/src/ForEmArrays.java
@@ -0,0 +1,13 @@
+// ExemploFor.java
+public class ForEmArrays {
+    public static void main(String[] args) {
+        //Arrays tipo alunos
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+    
+        for (int x=0; x<alunos.length; x++) {
+            
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    
+    }
+    }
